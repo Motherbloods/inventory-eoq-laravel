1d596a50826738e3ebaa09c4b2d9ff2f288b3e38
Motherbloods <habibskh06@gmail.com>
2026-04-11 16:30:20 +0700

    feat(user): register resource routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BahanBakuController;
 use App\Http\Controllers\PemasokController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -27,5 +28,11 @@ Route::middleware(['auth', 'active'])->group(function () {
 
         Route::resource('bahan-baku', BahanBakuController::class);
         Route::resource('pemasok', PemasokController::class);
+    });
+
+    Route::middleware('role:admin')->group(function () {
+
+        // Manajemen Pengguna
+        Route::resource('users', UserController::class);
     });
 });
